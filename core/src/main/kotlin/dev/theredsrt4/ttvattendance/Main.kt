1f87583b61b1ace54ff1a5e0100c60a60dc10fd9
TheRedSRT4 <FadeShotEdits@gmail.com>
2023-03-25 15:23:56 -0400

    Changed stuff for migration.

--- core/src/main/kotlin/dev/theredsrt4/ttvattendance/Main.kt
+++ core/src/main/kotlin/dev/theredsrt4/ttvattendance/Main.kt
@@ -8,13 +8,14 @@ fun main() {
         "jvlia",
         HttpApiHandler("0o2yfr1f5av3716vnnlyahb405acgd", "ip2n8hli2rmv6aar2sspkdgtt6xdnx", "ttva.secret"),
         Database.connect(
-            "jdbc:postgresql://137.184.217.45/",
+            "jdbc:postgresql://162.33.23.226/",
             driver = "org.postgresql.Driver",
             user = "postgres",
             password = "Cupcake12!"
         ),
         "ttva.ftl",
         120,
-        1
+        1,
+        "X9TV457sqHFYHVHD9rW3HXPW"
     )
 }
